3b5336cd902f93203fb35cea5212316b2b1e1471
Sankalp Bihani <sankalpbihani.008@gmail.com>
2016-03-16 20:41:08 +0530

    IDA* + modifications in A*

--- 1-a.py
+++ 1-a.py
@@ -1,5 +1,4 @@
 import heapq
-import copy
 
 N = 4
 
@@ -10,6 +9,7 @@ def getHeuristic(state):
 	for i in range(N):
 		for j in range(N):
 			val = state[i][j]
+
 			if val > 0:
 				heuristic += abs(i - val/N) + abs(j - val%N)
 	
@@ -45,10 +45,9 @@ def getMoves(state):
 
 	for i in range(4):
 		if isValidIndexIndex(x + dx[i], y + dy[i]):
-			newState = copy.deepcopy(state)
-			newState[x][y] = newState[x + dx[i]][y + dy[i]]
-			newState[x + dx[i]][y + dy[i]] = 0
-			moves.append(str(newState))
+			state[x][y], state[x + dx[i]][y + dy[i]] = state[x + dx[i]][y + dy[i]], state[x][y]
+			moves.append(str(state))
+			state[x][y], state[x + dx[i]][y + dy[i]] = state[x + dx[i]][y + dy[i]], state[x][y]
 
 	return moves
 
@@ -57,24 +56,21 @@ assert(len(getMoves(str([[2, 1, 3, 5], [4, 0, 6, 7], [8, 9, 10, 11], [12, 13, 14
 def AStar(start):
 	explored = set()
 	frontier = []
-	inFrontier = dict()
 
-	heapq.heappush(frontier, (getHeuristic(start), 0, start))
-	inFrontier[start] = getHeuristic(start)
+	heapq.heappush(frontier, (getHeuristic(start), getHeuristic(start), start))
 	generatedNodes = 0
 
 	while len(frontier):
-		fvalue, gvalue, state = heapq.heappop(frontier)
+		fvalue, hvalue, state = heapq.heappop(frontier)
+		gvalue = fvalue - hvalue
 
-		if isGoal(state):
-			explored.add(state)
-			return fvalue, len(explored), generatedNodes
-		
 		if state in explored:
 			continue
 
-		del inFrontier[state]
 		explored.add(state)
+
+		if isGoal(state):
+			return fvalue, len(explored), generatedNodes
 
 		for nextState in getMoves(state):
 			generatedNodes += 1
@@ -85,11 +81,51 @@ def AStar(start):
 			nextGvalue = gvalue + 1
 			nextFvalue = nextGvalue + getHeuristic(nextState)
 
-			if (nextState in inFrontier) and (inFrontier[nextState] < nextFvalue):
-				continue
-
-			heapq.heappush(frontier, (nextFvalue, nextGvalue, nextState))
-			inFrontier[nextState] = nextFvalue
+			heapq.heappush(frontier, (nextFvalue, getHeuristic(nextState), nextState))
 
 assert(AStar(str([[1, 2, 6, 3],[4, 9, 5, 7], [8, 13, 11, 15],[12, 14, 0, 10]])) == (11, 19, 59))
-print AStar(str([[15, 8, 10, 4], [9, 12, 11, 3], [0, 5, 2, 14], [7, 1, 6, 13]]))
+#print AStar(str([[15, 8, 10, 4], [9, 12, 11, 3], [0, 5, 2, 14], [7, 1, 6, 13]]))
+
+INF = 10**10
+FOUND = -10
+
+generatedNodes = 0
+def dfs(state, gvalue, flimit):
+	generatedNodes = 0
+	fvalue = gvalue + getHeuristic(state)
+
+	if fvalue > flimit:
+		return fvalue, 0
+
+	if isGoal(state):
+		return FOUND, 0
+
+	minVal = INF
+
+	for nextState in getMoves(state):
+		val, count = dfs(nextState, gvalue + 1, flimit)
+		generatedNodes += 1 + count
+
+		if val == FOUND:
+			return val, generatedNodes
+
+		if val < minVal:
+			minVal = val
+
+	return minVal, generatedNodes
+
+def IDAStar(start):
+	generatedNodes = 0
+	flimit = getHeuristic(start)
+
+	while True:
+		val, generatedNodes = dfs(start, 0, flimit)
+		
+		if val == FOUND:
+			return flimit, generatedNodes
+		
+		flimit = val
+
+assert(IDAStar(str([[1, 2, 6, 3],[4, 9, 5, 7], [8, 13, 11, 15],[12, 14, 0, 10]])) == (11, 25))
+#print IDAStar(str([[1,2,0,3],[4,5,6,7],[8,9,10,11],[12,13,14,15]]))
+
